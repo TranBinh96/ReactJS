efefdee07262eb3108293c4fe51ef4f15e6b9cdd
TranBinh96 <binhvinhphong@gmail.com>
2022-12-22 00:01:19 +0700

    dfdsf

---
+++ src/Application.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Application extends Component{
+    render(){
+        return (
+            <h1> safsdfsdfsdf</h1>
+        );
+    }
+}
+export default Application;
